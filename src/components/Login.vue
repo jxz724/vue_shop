fe52f3527e74cd867449f3d0adc9038694197f6d
dengchao <dengchao@aspirecn.com>
2022-09-04 12:13:28 +0800

    ssdrz

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login_content">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.login_content{
+  background-color: #2b4b6b;
+  height:100%;
+}
+.login_box{
+  width:450px;
+  height:300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform: translate(-50%,-50%);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
+import '@/assets/css/global.css'
 
 Vue.prototype.axios = axios
 Vue.use(ElementUI)
